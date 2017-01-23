ef2face9427e73f38059f9163327b0fe5468db14
Andrew Lau <andrew@andrewklau.com>
2017-01-23 21:02:41 +1100

    Initial

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HelloWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>()
+                        .UseUrls("http://*:8080")
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Dotnet Example
+
+Example repository for running dotnet on [AusNimbus](https://www.ausnimbus.com.au/) (OpenShift V3)
+
